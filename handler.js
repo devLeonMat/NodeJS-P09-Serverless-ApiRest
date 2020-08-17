a5250dcd08c087237cd90c36abf524f127ec3d9e
Leon Matias Roslin <leonmatias1991@gmail.com>
2020-08-16 22:01:57 -0500

    save change

--- handler.js
+++ handler.js
@@ -1,18 +1,42 @@
 'use strict';
 
 module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Serverless function get!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+    if (event.queryStringParameters && event.queryStringParameters.name) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: `Hello ${event.queryStringParameters.name}, nice to meet you!!`
+            }),
+        }
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: 'Serverless function get!',
+                input: event,
+            },
+            null,
+            2
+        ),
+    };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+
+module.exports.client = async event => {
+    let json = JSON.parse(event.body);
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: 'Hi I have received a JSON object from you!',
+                object: json
+            },
+            null,
+            2
+        ),
+    };
 };
